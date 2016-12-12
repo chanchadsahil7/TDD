49d755a16dff5a4fe8fa92c91d76d1fd567b5e28
sahil <chanchadsahil@gmail.com>
2016-12-12 08:55:10 +0530

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ class NewVisitorTest(unittest.TestCase):  #1
 
     def setUp(self):  #2
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):  #3
         self.browser.quit()
@@ -13,14 +14,13 @@ class NewVisitorTest(unittest.TestCase):  #1
     def test_can_start_a_list_and_retrieve_it_later(self):  #4
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
+        print("Testing function call")
         self.browser.get('http://localhost:8000')
-        self.browser.implicitly_wait(3)
-
+        print("Testing function call2")
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        #self.fail('Finish the test!')  #6
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -28,13 +28,13 @@ class NewVisitorTest(unittest.TestCase):  #1
                 inputbox.get_attribute('placeholder'),
                 'Enter a to-do item'
         )
-        
-		# She types "Buy peacock feathers" into a text box (Edith's hobby
-		# is tying fly-fishing lures)
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-		# When she hits enter, the page updates, and now the page lists
-		# "1: Buy peacock feathers" as an item in a to-do list
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
@@ -43,8 +43,9 @@ class NewVisitorTest(unittest.TestCase):  #1
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
 
-		# There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
         self.fail('Finish the test!')
 
 		# The page updates again, and now shows both items on her list
