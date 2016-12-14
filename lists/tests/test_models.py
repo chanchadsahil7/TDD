bb1bd5bb0da684dc7222ececceeed4695be554a4
sahil <chanchadsahil@gmail.com>
2016-12-14 15:25:10 +0530

    Adjust new list view to do model validation

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -8,6 +8,7 @@ from lists.models import Item, List'''
 
 from django.test import TestCase
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 
 class ListAndItemModelTest(TestCase):
@@ -40,3 +41,12 @@ class ListAndItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
 		self.assertEqual(second_saved_item.list, list_)
+
+
+	def test_cannot_save_empty_list_items(self):
+
+		list_ = List.objects.create()
+		item = Item(list=list_, text='')
+		with self.assertRaises(ValidationError):     	
+			item.save()
+			item.full_clean()
